418fd77c7f44cf7f78187f59789f3e5d2e3f43f6
KennyTV <jahnke.nassim@gmail.com>
2020-08-17 21:53:57 +0200

    Some initial changes to the template

---
+++ src/main/java/eu/kennytv/upsidedown/UpsidedownMod.java
@@ -0,0 +1,10 @@
+package eu.kennytv.upsidedown;
+
+import net.fabricmc.api.ModInitializer;
+
+public final class UpsidedownMod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
